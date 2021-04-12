22e46fcdbdce54705426d1b845f9ee1f2633f793
LowerLegs <hsuk0409@gmail.com>
2021-04-13 07:08:20 +0900

    꼬리 재귀 스택 프레임 디버깅

---
+++ oop-study/src/test/java/com/oop/study/oopstudy/recursion/TailRecursionTest.java
@@ -0,0 +1,27 @@
+package com.oop.study.oopstudy.recursion;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TailRecursionTest {
+
+    @Test
+    @DisplayName("스택 프레임 몇개가 쌓이는지 디버깅 해보기")
+    public void tailRecursion() {
+        puff(4, 6, 4);
+    }
+
+    public void puff(int n, int a, int b) {
+        if (n <= 0) {
+            return;
+        }
+
+        log.info(String.format("%d %d %d", n, a, b));
+
+        puff(n - 3, a, b + n);
+        puff(n - 1, b, a + n);
+        puff(n - 2, b, a + n);
+    }
+}
