92923f67cb2b7ec5f057420239ea55111f412c4c
LowerLegs <hsuk0409@gmail.com>
2021-04-26 09:06:12 +0900

    호어 분할법을 이용한 퀵 정렬 구현

---
+++ oop-study/src/test/java/com/oop/study/oopstudy/sort/quick/QuickSortHoarePartitionTest.java
@@ -0,0 +1,58 @@
+package com.oop.study.oopstudy.sort.quick;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class QuickSortHoarePartitionTest {
+
+    @DisplayName("호어 분할법을 이용한 퀵 정렬")
+    @Test
+    public void quickSortHoarePartition() {
+        int[] arr = new int[]{ 3, 1, 5, 7, 2, 9, 8, 4 };
+
+        printArr(arr);
+        quickSortRecursive(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+
+    private void quickSortRecursive(int[] arr, int l, int r) {
+        if (l >= r) return;
+
+        int pivotPos = partitionHoare(arr, l, r);
+
+        quickSortRecursive(arr, l, pivotPos - 1);
+        quickSortRecursive(arr, pivotPos + 1, r);
+    }
+
+    private int partitionHoare(int[] arr, int l, int r) {
+        int pivot = arr[l];
+
+        int i = l + 1;
+        int j = r;
+        while (i <= j) {
+            while (i < r && arr[i] < pivot) {
+                ++i;
+            }
+            while (j > l && arr[j] > pivot) {
+                --j;
+            }
+            if (i >= j) break;
+            swap(arr, i, j);
+        }
+        swap(arr, l, j);
+        return j;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    private void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
