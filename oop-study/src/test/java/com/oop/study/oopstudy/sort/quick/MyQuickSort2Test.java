6e35b7dba571624423ab2f8a4de372b4fc825a20
LowerLegs <hsuk0409@gmail.com>
2021-05-25 08:46:13 +0900

    코드 정리!

--- oop-study/src/test/java/com/oop/study/oopstudy/sort/quick/MyQuickSort2Test.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/sort/quick/MyQuickSort2Test.java
@@ -12,7 +12,9 @@ public class MyQuickSort2Test {
     public void sortQuickSecondsTest() {
         int[] arr = new int[] { 5, 8, 4, 5, 11, 2, 13, 1 };
 
+        printArr(arr);
         myQuickSortRecursive(arr, 0, arr.length - 1);
+        printArr(arr);
     }
 
     private void myQuickSortRecursive(int[] arr, int left, int right) {
@@ -28,24 +30,28 @@ public class MyQuickSort2Test {
     }
 
     private int partition(int[] arr, int left, int right) {
-        int pivot = arr[right];
-
-        int swapIndex = (left - 1);
+        int swapIndex = left;
         for (int i = left; i < right; ++i) {
-            if (arr[i] < pivot) {
-                ++swapIndex;
+            if (arr[i] < arr[right]) {
                 swap(arr, swapIndex, i);
+                ++swapIndex;
             }
         }
-        int pivotPos = (swapIndex + 1);
-        swap(arr, pivotPos, right);
+        swap(arr, swapIndex, right);
 
-        return pivotPos;
+        return swapIndex;
     }
 
     private void swap(int[] arr, int left, int right) {
         int tmp = arr[left];
         arr[left] = arr[right];
         arr[right] = tmp;
+    }
+
+    private void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
