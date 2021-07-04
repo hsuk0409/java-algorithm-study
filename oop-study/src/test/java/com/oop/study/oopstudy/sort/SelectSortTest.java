334125d4190b87df3d169242416b184e0599d9fa
LowerLegs <hsuk0409@gmail.com>
2021-07-04 15:30:13 +0900

    코드 정리

--- oop-study/src/test/java/com/oop/study/oopstudy/sort/SelectSortTest.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/sort/SelectSortTest.java
@@ -12,9 +12,10 @@ public class SelectSortTest {
     @DisplayName("오름차순 선책 정렬")
     @Test
     void selectSortTest() {
-        int[] arr = new int[]{ 4, 6, 11, 15, 2, 5, 1, 3 };
+        int[] arr = new int[]{4, 6, 11, 15, 2, 5, 1, 3};
 
         selectSort(arr);
+
         assertThat(arr[0]).isEqualTo(1);
     }
 
@@ -30,7 +31,7 @@ public class SelectSortTest {
     @DisplayName("최소값의 인덱스 가져오기")
     @Test
     void testGetMinIndex() {
-        int[] arr = new int[]{ 4, 6, 11, 15, 2, 5, 1, 3 };
+        int[] arr = new int[]{4, 6, 11, 15, 2, 5, 1, 3};
         int minIndex = getIndexOfMinValue(arr, 0);
 
         assertThat(minIndex).isEqualTo(6);
@@ -38,11 +39,13 @@ public class SelectSortTest {
 
     private int getIndexOfMinValue(int[] arr, int start) {
         int ret = start;
+
         for (int i = start + 1; i < arr.length; ++i) {
             if (arr[ret] > arr[i]) {
                 ret = i;
             }
-         }
+        }
+
         return ret;
     }
 }
