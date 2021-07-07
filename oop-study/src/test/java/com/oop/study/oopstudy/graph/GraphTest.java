041f37e91e31f1a028574fb2698dc1753aa7656b
LowerLegs <hsuk0409@gmail.com>
2021-07-08 00:15:34 +0900

    임포트 정리

--- oop-study/src/test/java/com/oop/study/oopstudy/graph/GraphTest.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/graph/GraphTest.java
@@ -1,11 +1,9 @@
 package com.oop.study.oopstudy.graph;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class GraphTest {
 
