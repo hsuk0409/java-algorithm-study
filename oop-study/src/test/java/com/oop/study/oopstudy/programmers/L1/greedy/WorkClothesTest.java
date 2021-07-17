919f1b82dcd834a0e2612701b7043aa9babc52e5
LowerLegs <hsuk0409@gmail.com>
2021-07-17 12:17:52 +0900

    그리디 체육복 풀이 테스트

--- oop-study/src/test/java/com/oop/study/oopstudy/programmers/L1/greedy/WorkClothesTest.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/programmers/L1/greedy/WorkClothesTest.java
@@ -1,8 +1,22 @@
 package com.oop.study.oopstudy.programmers.L1.greedy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class WorkClothesTest {
+
+    @DisplayName("그리디 체육복 문제")
+    @Test
+    void test() {
+        int n = getOptimizationShareCount(10, new int[]{5, 4, 3, 2, 1}, new int[]{3, 1, 2, 5, 4});
+
+        assertThat(n, is(10));
+    }
 
     public int getOptimizationShareCount(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;
