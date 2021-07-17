51e550dc6914067c41b3eefc821f9a18e690a4bb
LowerLegs <hsuk0409@gmail.com>
2021-07-17 12:11:24 +0900

    프로그래머스 체육복 문제, 블로그 코드 참고해서 수정

--- oop-study/src/test/java/com/oop/study/oopstudy/programmers/L1/greedy/WorkClothesTest.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/programmers/L1/greedy/WorkClothesTest.java
@@ -1,23 +1,31 @@
 package com.oop.study.oopstudy.programmers.L1.greedy;
 
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class WorkClothesTest {
 
     public int getOptimizationShareCount(int m, int[] lost, int[] reserve) {
-        int answer = reserve.length == 1 ? 2 : reserve.length;
-        HashSet<Integer> discovered = new HashSet<>();
+        int answer = n - lost.length;
         Arrays.sort(lost);
         Arrays.sort(reserve);
 
-        for (int i = 0; i < reserve.length; ++i) {
-            for (int k = 0; k < lost.length; ++k) {
-                if (lost[k] == (reserve[i] - 1) || lost[k] == (reserve[i] + 1)) {
-                    if (!discovered.contains(lost[k])) {
-                        discovered.add(lost[k]);
-                        ++answer;
-                    }
+        for (int i = 0; i < lost.length; ++i) {
+            for (int k = 0; k < reserve.length; ++k) {
+                if (lost[i] == reserve[k]) {
+                    ++answer;
+                    lost[i] = -1;
+                    reserve[k] = -1;
+                    break;
+                }
+            }
+        }
+
+        for (int j : lost) {
+            for (int k = 0; k < reserve.length; ++k) {
+                if ((j - 1) == reserve[k] || (j + 1) == reserve[k]) {
+                    ++answer;
+                    reserve[k] = -1;
+                    break;
                 }
             }
         }
