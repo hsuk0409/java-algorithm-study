32168255c1b4c104b0cbafca79bce27d9a59b748
LowerLegs <hsuk0409@gmail.com>
2021-08-10 19:26:31 +0900

    KMP 알고리듬 수정

--- oop-study/src/test/java/com/oop/study/oopstudy/string/KMPAlgoTest.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/string/KMPAlgoTest.java
@@ -1,9 +1,21 @@
 package com.oop.study.oopstudy.string;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class KMPAlgoTest {
+    @DisplayName("KMP 알고리듬")
+    @Test
+    void test() {
+        String str = "aaaabaaaac";
+        String patten = "aaaac";
+
+        List<Integer> result = kmpSearch(str, patten);
+        assert (result.get(0) == 6);
+    }
 
     /** 커누스-모리스-플랫
      * H: haystack (짚더미)
@@ -15,24 +27,44 @@ public class KMPAlgoTest {
         int nLen = N.length();
 
         List<Integer> result = new ArrayList<>();
-        List<Integer> pi = getPartialMatch(N);
-        int begin = 0;
+        int[] pi = getPartialMatch(N);
+        char[] s = H.toCharArray();
+        char[] p = N.toCharArray();
         int matched = 0;
 
-        while (begin <= hLen - nLen) {
-            if (matched < nLen && H.charAt(begin + matched) == N.charAt(matched)) {
-                ++matched;
-                if (matched == nLen) result.add(begin);
-            } else {
-                begin += matched - pi.get(matched - 1);
-                matched = pi.get(matched - 1);
+        for (int i = 0; i < hLen; ++i) {
+            while (matched > 0 && s[i] != p[matched]) {
+                matched = pi[matched - 1];
+            }
+
+            if (s[i] == p[matched]) {
+                if (matched == nLen - 1) {
+                    result.add(i - nLen + 1);
+                    matched = pi[matched];
+                } else {
+                    ++matched;
+                }
             }
         }
 
         return result;
     }
 
-    private List<Integer> getPartialMatch(String str) {
-        return null;
+    private int[] getPartialMatch(final String pattern) {
+        int len = pattern.length();
+        int[] pi = new int[len];
+
+        int matched = 0;
+
+        for (int i = 0; i < len; ++i) {
+            while (matched > 0 && pattern.charAt(i) != pattern.charAt(matched)) {
+                matched = pi[matched - 1];
+            }
+            if (pattern.charAt(i) == pattern.charAt(matched)) {
+                pi[i] = ++matched;
+            }
+        }
+
+        return pi;
     }
 }
