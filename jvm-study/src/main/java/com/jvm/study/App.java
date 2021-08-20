22efb6bdc21ac7c531809bf7e5447cfbc4c2b67a
LowerLegs <hsuk0409@gmail.com>
2021-08-20 16:20:39 +0900

    jvm 강의에 사용할 프로젝트 셋팅

---
+++ jvm-study/src/main/java/com/jvm/study/App.java
@@ -0,0 +1,11 @@
+package com.jvm.study;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ClassLoader classLoader = App.class.getClassLoader();
+        System.out.println(classLoader);
+    }
+}
