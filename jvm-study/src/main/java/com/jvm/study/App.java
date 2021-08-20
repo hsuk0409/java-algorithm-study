78c022bb11e70f090d6c74bf299bbcb51f88bf3a
LowerLegs <hsuk0409@gmail.com>
2021-08-20 16:22:18 +0900

    App.class 의 클래스 로더 부모들을 출력해보기

--- jvm-study/src/main/java/com/jvm/study/App.java
+++ jvm-study/src/main/java/com/jvm/study/App.java
@@ -1,11 +1,10 @@
 package com.jvm.study;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         ClassLoader classLoader = App.class.getClassLoader();
         System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
     }
 }
