e42a682ee6f340e7ccf295ed4f5a58ee07ae2702
ChoinSuk <hsuk0409@gmail.com>
2021-09-04 17:18:46 +0900

    spring-mongodb 연동 테스트

---
+++ spring-mongod-study/src/main/java/com/mongod/study/SpringMongodStudyApplication.java
@@ -0,0 +1,13 @@
+package com.mongod.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodStudyApplication.class, args);
+	}
+
+}
