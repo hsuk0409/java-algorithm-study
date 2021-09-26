c60bc0ed0fdf79a7cf7b563fea24c17086e2dcbf
ChoinSuk <hsuk0409@gmail.com>
2021-09-26 17:08:50 +0900

    AppConfig 리팩토링

--- spring-core-study/src/main/java/com/spring/corestudy/AppConfig.java
+++ spring-core-study/src/main/java/com/spring/corestudy/AppConfig.java
@@ -1,6 +1,8 @@
 package com.spring.corestudy;
 
+import com.spring.corestudy.discount.DiscountPolicy;
 import com.spring.corestudy.discount.FixDiscountPolicy;
+import com.spring.corestudy.member.MemberRepository;
 import com.spring.corestudy.member.MemberService;
 import com.spring.corestudy.member.MemberServiceImpl;
 import com.spring.corestudy.member.MemoryMemberRepository;
@@ -9,10 +11,18 @@ import com.spring.corestudy.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
