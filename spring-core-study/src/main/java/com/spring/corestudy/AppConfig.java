03b420291f0610bfd2f6a1422ccb2adbd463ab8a
ChoinSuk <hsuk0409@gmail.com>
2021-09-28 20:28:14 +0900

    AppConfig에 Configuration과 Bean 아노테이션을 추가해서 스프링 컨테이너에 등록하고 개체 생성하여 사용하기

--- spring-core-study/src/main/java/com/spring/corestudy/AppConfig.java
+++ spring-core-study/src/main/java/com/spring/corestudy/AppConfig.java
@@ -1,7 +1,6 @@
 package com.spring.corestudy;
 
 import com.spring.corestudy.discount.DiscountPolicy;
-import com.spring.corestudy.discount.FixDiscountPolicy;
 import com.spring.corestudy.discount.RateDiscountPolicy;
 import com.spring.corestudy.member.MemberRepository;
 import com.spring.corestudy.member.MemberService;
@@ -9,20 +8,28 @@ import com.spring.corestudy.member.MemberServiceImpl;
 import com.spring.corestudy.member.MemoryMemberRepository;
 import com.spring.corestudy.order.OrderService;
 import com.spring.corestudy.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- spring-core-study/src/test/java/com/spring/corestudy/member/MemberServiceTest.java
+++ spring-core-study/src/test/java/com/spring/corestudy/member/MemberServiceTest.java
@@ -5,20 +5,24 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberServiceTest {
-    MemberService memberService;
+//    MemberService memberService;
 
     @BeforeEach
     void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        memberService = appConfig.memberService();
     }
 
     @DisplayName("멤버가 정상적으로 등록되는지 테스트")
     @Test
     void join() {
         //given
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = context.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
 
         //when
