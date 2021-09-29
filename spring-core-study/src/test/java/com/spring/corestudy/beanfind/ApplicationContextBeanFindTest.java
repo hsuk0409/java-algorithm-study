32a066911c0521e944dd5e33eeb576ea53bf6fef
ChoinSuk <hsuk0409@gmail.com>
2021-09-29 19:18:45 +0900

    빈 이름으로 조회하기, 해당하는 빈 없을 경우 익셉션 에러 테스트

---
+++ spring-core-study/src/test/java/com/spring/corestudy/beanfind/ApplicationContextBeanFindTest.java
@@ -0,0 +1,45 @@
+package com.spring.corestudy.beanfind;
+
+import com.spring.corestudy.AppConfig;
+import com.spring.corestudy.member.MemberService;
+import com.spring.corestudy.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBeanFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @DisplayName("빈 이름으로 조회")
+    @Test
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("빈 이름 없이 타입으로만 조회")
+    @Test
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("빈을 구체 타입으로 조회")
+    @Test
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("빈 이름으로 조회 안되는 경우")
+    @Test
+    void findBeanByNameFail() {
+//        MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+}
