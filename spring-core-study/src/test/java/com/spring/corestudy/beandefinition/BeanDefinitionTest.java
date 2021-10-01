37970bbe7e6c249e7f8f56b46f32726e19e147bb
ChoinSuk <hsuk0409@gmail.com>
2021-10-01 21:36:57 +0900

    푸시 테스트

--- spring-core-study/src/test/java/com/spring/corestudy/beandefinition/BeanDefinitionTest.java
+++ spring-core-study/src/test/java/com/spring/corestudy/beandefinition/BeanDefinitionTest.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class BeanDefinitionTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig .class);
+//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.class");
 
     @DisplayName("빈 설정 메타정보 확인 (**중요:: BeanDefinition 추상화)")
     @Test
