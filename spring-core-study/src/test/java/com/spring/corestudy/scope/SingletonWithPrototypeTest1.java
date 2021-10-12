33a1c85e46819971c24e5a2f135b3fc1daea57a2
ChoinSuk <hsuk0409@gmail.com>
2021-10-12 21:54:26 +0900

    프로토타입 빈과 싱글톤 빈 같이 사용 시 Spring의 ObjectProvider를 사용해여 개체 여러개 생성

--- spring-core-study/src/test/java/com/spring/corestudy/scope/SingletonWithPrototypeTest1.java
+++ spring-core-study/src/test/java/com/spring/corestudy/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,8 @@ package com.spring.corestudy.scope;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -15,6 +17,8 @@ public class SingletonWithPrototypeTest1 {
     /**
      * 프로토타입 빈과 싱글톤 빈을 같이 사용 시 원하는 로직을 만들기 힘들다.
      * --> Dependency Lookup (의존관계 조회)로 해결
+     * 스프링에서 제공해주는 ObjectProvider 사용 --> 항상 새로운 개체가 만들어진다.
+     * ** 핵심은 직접 스프링 컨테이너에 요청하지 않고 대신 요청해준다. **
      */
 
     @DisplayName("프로토타입 빈은 주입 때마다 개체를 새로 생성한다.")
@@ -40,18 +44,22 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
