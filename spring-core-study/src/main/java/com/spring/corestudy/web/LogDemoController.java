ae70a40f645898e8dc8a9f41c9fb0e6657c84806
ChoinSuk <hsuk0409@gmail.com>
2021-10-14 20:08:14 +0900

    ObjectProvider를 이용해 MyLogger가 request 없이도 빈 주입이 가능하도록 설정

--- spring-core-study/src/main/java/com/spring/corestudy/web/LogDemoController.java
+++ spring-core-study/src/main/java/com/spring/corestudy/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.spring.corestudy.web;
 
 import com.spring.corestudy.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     public String logDemo(HttpServletRequest request) {
         String requestUrl = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestUrl(requestUrl);
 
         myLogger.log("controller test");
