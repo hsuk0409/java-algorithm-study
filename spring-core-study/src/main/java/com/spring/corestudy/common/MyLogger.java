aad14e485cfa02c5bba57eb6196fa8604bf48137
ChoinSuk <hsuk0409@gmail.com>
2021-10-16 10:48:33 +0900

    프록시로 해당 개체를 빈으로 미리 등록해놓고 실제 사용할 때까지 지연처리 적용

--- spring-core-study/src/main/java/com/spring/corestudy/common/MyLogger.java
+++ spring-core-study/src/main/java/com/spring/corestudy/common/MyLogger.java
@@ -1,6 +1,7 @@
 package com.spring.corestudy.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,11 +9,20 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 /**
- * 스프링 컨테이너에 요청하는 시점에 빈이 생성된다.
+ * 스프링 컨테이너에 요청(request)하는 시점에 빈이 생성된다.
  * HTTP 요청 당 하나씩 생성된다.
+ *
+ * ScopedProxyMode.TARGET_CLASS 설정하므로
+ * 웹 애플리케이션 실행 시점에 프록시를 생성해서 빈 주입 해준다.
+ * -> ObjectProvider 또는 위 기능 사용하지 않으면 빈 주입 불가능
+ * 실제 요청이 오면 그때 내부에 실제 빈을 요청하는 위임 로직이 들어있다.
+ *
+ * ******** 핵심은 Provider, Proxy 둘다 실제 개체를 사용하기 전까지 지연처리를 해주는 것이다!! ********
+ *
+ * 애노케이션 설정 변경만으로 원본 개체를 조작할 수 있는 것이 다형성과 DI 컨테이너의 가장 큰 힘이다.
  */
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
     private String uuid;
     private String requestUrl;
--- spring-core-study/src/main/java/com/spring/corestudy/web/LogDemoController.java
+++ spring-core-study/src/main/java/com/spring/corestudy/web/LogDemoController.java
@@ -2,7 +2,6 @@ package com.spring.corestudy.web;
 
 import com.spring.corestudy.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     public String logDemo(HttpServletRequest request) {
         String requestUrl = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestUrl(requestUrl);
 
         myLogger.log("controller test");
