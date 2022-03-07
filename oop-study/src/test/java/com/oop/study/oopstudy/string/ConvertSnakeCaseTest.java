06b09b21a4bce785295d12e7c83e2b91e2370c64
chionsuk <hsuk0409@gmail.com>
2022-03-07 21:28:43 +0900

    스네이크 케이스 변환 테스트 작성

--- oop-study/src/test/java/com/oop/study/oopstudy/string/ConvertSnakeCaseTest.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/string/ConvertSnakeCaseTest.java
@@ -3,11 +3,25 @@ package com.oop.study.oopstudy.string;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ConvertSnakeCaseTest {
 
     @DisplayName("띄어씌기와 카멜 케이스를 스네이크 케이스로 변환한다.")
     @Test
     void convertSnakeCaseTest() {
-        
+        //given
+        String str = "miero fiberZon mat tang";
+        String expected = "MIERO_FIBER_ZON_MAT_TANG";
+
+        //when
+        convertToSnakeCase(str);
+
+        //then
+        assertThat(str).isEqualTo(expected);
+    }
+
+    private void convertToSnakeCase(String str) {
+
     }
 }
