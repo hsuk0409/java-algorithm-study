feac4e31fee1151146f0e339f33d05f4ba4b099d
chionsuk <hsuk0409@gmail.com>
2022-07-03 00:28:27 +0900

    문자열 쌍제거 스택 이용

--- oop-study/src/test/java/com/oop/study/oopstudy/string/StringCoupleRemoveTest.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/string/StringCoupleRemoveTest.java
@@ -3,6 +3,8 @@ package com.oop.study.oopstudy.string;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 public class StringCoupleRemoveTest {
 
     @DisplayName("문자열 짝지어 제거하기 문제")
@@ -20,5 +22,26 @@ public class StringCoupleRemoveTest {
                 }
             }
         }
+    }
+
+    @DisplayName("문자열 짝지어 제거하기 문제, 스택 이용")
+    @Test
+    void test2() {
+        String s = "baabaa";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (stack.empty()) {
+                stack.push(s.charAt(i));
+                continue;
+            }
+            if (stack.peek().equals(s.charAt(i))) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        System.out.println(stack);
     }
 }
