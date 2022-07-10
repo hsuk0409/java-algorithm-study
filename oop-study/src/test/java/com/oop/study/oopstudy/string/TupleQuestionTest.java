3d33f1ded6bc9f4cc3f71d7922a4c78fba949bea
chionsuk <hsuk0409@gmail.com>
2022-07-11 00:24:26 +0900

    카카오 겨울 인턴십 튜플 문제 다른사람 풀이 가져와서 디버깅 후 리팩토링

--- oop-study/src/test/java/com/oop/study/oopstudy/string/TupleQuestionTest.java
+++ oop-study/src/test/java/com/oop/study/oopstudy/string/TupleQuestionTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TupleQuestionTest {
 
     @DisplayName("문자열 그룹 중 적은 수 순서대로 중복없이 그룹핑하기")
@@ -23,7 +25,7 @@ public class TupleQuestionTest {
                 ++i;
                 StringBuilder numStr = new StringBuilder();
                 while (s3.charAt(i) != '}') {
-                    if (s3.charAt(i) != ','){
+                    if (s3.charAt(i) != ',') {
                         numStr.append(s3.charAt(i));
                     } else {
                         tmpNums.add(Integer.parseInt(numStr.toString()));
@@ -55,8 +57,36 @@ public class TupleQuestionTest {
         sortArrArr(tmpStorages);
 
 
-
         System.out.println();
+    }
+
+    @DisplayName("문자열 그룹 중 적은 수 순서대로 중복없이 그룹핑하기 (다른사람 코드)")
+    @Test
+    void tupleQuestionTest2() {
+        String s = "{{20,111},{111}}";
+
+        Set<String> set = new HashSet<>();
+        String tmpStr1 = s.replaceAll("[{]", " ");
+        String tmpStr2 = tmpStr1.replaceAll("[}]", " ");
+        String[] arr = tmpStr2.trim().split(" , ");
+        Arrays.sort(arr, Comparator.comparingInt(String::length));
+        int[] result = new int[arr.length];
+        int resultInx = 0;
+        for (String s1 : arr) {
+            for (String s2 : s1.split(",")) {
+                if (set.add(s2)) {
+                    result[resultInx++] = Integer.parseInt(s2);
+                }
+            }
+        }
+
+        List<Integer> expect = new ArrayList<>();
+        expect.add(111);
+        expect.add(20);
+
+        for (int i = 0; i < result.length; ++i) {
+            assertThat(result[i]).isEqualTo(expect.get(i));
+        }
     }
 
     public void sortArrArr(ArrayList<ArrayList<Integer>> arrArr) {
